c0784166a044665658710b136b12a18280288fb5
robsonsilv4 <robsonsilv410@gmail.com>
2020-01-28 07:27:50 -0300

    Adicionado footer

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function footer() {
+  return (
+    <footer className="main-footer">
+      <strong>
+        Copyright &copy; 2019
+        <a href="https://github.com/robsonsilv4/" target="_blank">
+          <span> Robson Silva</span>
+        </a>
+      </strong>
+    </footer>
+  );
+}
