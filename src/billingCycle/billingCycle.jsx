016e834f7252946dbffb3a3eab23e9c3bb98469b
robsonsilv4 <robsonsilv410@gmail.com>
2020-01-29 09:48:55 -0300

    Controle de tabs visíveis

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -9,11 +9,12 @@ import TabsHeader from '../common/tab/tabsHeader';
 import TabsContent from '../common/tab/tabsContent';
 import TabHeader from '../common/tab/tabHeader';
 import TabContent from '../common/tab/tabContent';
-import { selectTab } from '../common/tab/tabActions';
+import { selectTab, showTabs } from '../common/tab/tabActions';
 
 class BillingCycle extends Component {
   componentWillMount() {
     this.props.selectTab('tabList');
+    this.props.showTabs('tabList', 'tabCreate');
   }
 
   render() {
@@ -50,6 +51,6 @@ class BillingCycle extends Component {
 }
 
 const mapDispatchToPros = dispatch =>
-  bindActionCreators({ selectTab }, dispatch);
+  bindActionCreators({ selectTab, showTabs }, dispatch);
 
 export default connect(null, mapDispatchToPros)(BillingCycle);
