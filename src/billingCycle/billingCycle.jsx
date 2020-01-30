e09af6123a956967bdfdcd0495880785864d95d9
robsonsilv4 <robsonsilv410@gmail.com>
2020-01-30 08:50:29 -0300

    Alteração de despesas

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -12,7 +12,7 @@ import TabContent from '../common/tab/tabContent';
 import { selectTab, showTabs } from '../common/tab/tabActions';
 import List from './billingCycleList';
 import Form from './billingCycleForm';
-import { create } from './billingCycleActions';
+import { create, update } from './billingCycleActions';
 
 class BillingCycle extends Component {
   componentWillMount() {
@@ -40,7 +40,7 @@ class BillingCycle extends Component {
                 <Form onSubmit={this.props.create} />
               </TabContent>
               <TabContent id="tabUpdate">
-                <Form />
+                <Form onSubmit={this.props.update} />
               </TabContent>
               <TabContent id="tabDelete">
                 <h1>Excluir</h1>
@@ -54,6 +54,6 @@ class BillingCycle extends Component {
 }
 
 const mapDispatchToPros = dispatch =>
-  bindActionCreators({ selectTab, showTabs, create }, dispatch);
+  bindActionCreators({ selectTab, showTabs, create, update }, dispatch);
 
 export default connect(null, mapDispatchToPros)(BillingCycle);
